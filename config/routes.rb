dc860f8f317ed985bb8b605ab8cd98a92c141880
sagar-miglani <smiglani1994@gmail.com>
2017-03-20 20:12:48 -0700

    changed exercise index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "users#index"
+  get "/users" => "users#index"
+  get "/exercises" => "exercises#index"
 
   namespace :api do
   	namespace :v1 do
